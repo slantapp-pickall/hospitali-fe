f80002de130b54a882b33ff92d0da1c49b55dbb1
Onoja Victor <onoja.jsdev@gmail.com>
2023-09-18 22:53:15 +0100

    feat: initial style file configuration.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
-    <Route path='/' element="Here" />
+    <Route path='/' element={<NavBar/>} />
    </Routes>
    </BrowserRouter>
   );
