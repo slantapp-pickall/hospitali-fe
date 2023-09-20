b9977f5721cbc5c75c0346f61fd7cb0270055f36
Onoja Victor <onoja.jsdev@gmail.com>
2023-09-20 11:58:17 +0100

    feat: Add landing page components

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,35 @@
+import Articles from "../components/Articles";
+import CarRentals from "../components/CarRentals";
+import FeaturedListing from "../components/FeaturedListing";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
+import OurReviews from "../components/OurReviews";
+import Recommendations from "../components/Recommendations";
+import Subscribe from "../components/Subscribe";
+import "../styles/landingPage.scss";
+
+export default function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      <NavBar />
+
+      <div className="app-body">
+        <Hero />
+        <div id="property">
+          <Recommendations />
+          <FeaturedListing />
+        </div>
+        <CarRentals/>
+        <div id="about">
+          <OurReviews />
+        </div>
+        <div id="articles">
+          <Articles />
+        </div>
+        <Subscribe />
+        <Footer />
+      </div>
+    </div>
+  );
+}
