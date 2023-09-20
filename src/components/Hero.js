6785ce9306f0db1127056ec832b98e346422bd5c
Onoja Victor <onoja.jsdev@gmail.com>
2023-09-20 12:31:39 +0100

    style: remove blur on scrool, add background color

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,53 +1,73 @@
-import {AiOutlineRight} from "react-icons/ai"
-import partner1 from "../assets/icons/partner1.svg"
-import partner2 from "../assets/icons/partner2.svg"
-import partner3 from "../assets/icons/partner3.svg"
-import partner4 from "../assets/icons/partner4.svg"
+import { AiOutlineRight } from "react-icons/ai";
+import partner1 from "../assets/icons/partner1.svg";
+import partner2 from "../assets/icons/partner2.svg";
+import partner3 from "../assets/icons/partner3.svg";
+import partner4 from "../assets/icons/partner4.svg";
+import { useEffect, useState } from "react";
 export default function Hero() {
+  const [isBackgroundColored, setIsBackgroundColored] = useState(false);
+
+  const handleScroll = () => {
+    const threshold = 150;
+    setIsBackgroundColored(window.scrollY > threshold);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="hero-container">
-      <div className="text-section">
-     <div className="content-width">
-           <header>
-          Find The Place To Live <span className="your-dreams">Your Dreams</span> Easily Here
-        </header>
+      <div
+        className={
+          !isBackgroundColored ? "text-section" : "text-section hide-blur"
+        }
+      >
+        <div className="content-width">
+          <header>
+            Find The Place To Live{" "}
+            <span className="your-dreams">Your Dreams</span> Easily Here
+          </header>
 
-        <p>Everything you need about finding your place to live will be here, where it will be easier for you</p>
+          <p>
+            Everything you need about finding your place to live will be here,
+            where it will be easier for you
+          </p>
 
+          <div className="search-section">
+            <button>
+              Search <AiOutlineRight />{" "}
+            </button>
+            <input
+              type="text"
+              placeholder="Search for apartments in your desired location"
+            />
+          </div>
 
-<div className="search-section">
-    <button>Search <AiOutlineRight/> </button>
-    <input type="text" placeholder="Search for apartments in your desired location" />
-</div>
+          <div className="partners-section">
+            Our Partnership
+            <div className="partners-list">
+              <div className="partner">
+                <img src={partner1} alt="" />
+              </div>
 
+              <div className="partner">
+                <img src={partner2} alt="" />
+              </div>
 
-<div className="partners-section">
-    Our Partnership
-    <div className="partners-list">
+              <div className="partner">
+                <img src={partner3} alt="" />
+              </div>
 
-
-        <div className="partner">
-            <img src={partner1} alt="" />
+              <div className="partner">
+                <img src={partner4} alt="" />
+              </div>
+            </div>
+          </div>
         </div>
-        
-        <div className="partner">
-            <img src={partner2} alt="" />
-        </div>
-        
-        <div className="partner">
-            <img src={partner3} alt="" />
-        </div>
-        
-        <div className="partner">
-            <img src={partner4} alt="" />
-        </div>
-    </div>
-</div>
-
-
-
-     </div>
-
       </div>
 
       <div className="image-section"></div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import "../styles/navBar.scss";
 import { RxCaretDown } from "react-icons/rx";
 import logo from "../assets/icons/logo.svg";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Prompt from "../pages/Prompt";
 
@@ -11,42 +11,61 @@ export default function NavBar() {
   function handleToggle() {
     setToggleMenu(!toggleMenu);
   }
+ 
 
-const navigate = useNavigate()
+  const [getStarted, setGetStarted] = useState(false);
 
-const [getStarted, setGetStarted] = useState(false)
+ 
 
+  const [isBackgroundColored, setIsBackgroundColored] = useState(false);
+
+  const handleScroll = () => {
+    const threshold = 150;
+    setIsBackgroundColored(window.scrollY > threshold);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav className="nav-bar-container">
-      <div className="logo-section">
-        <div className="logo">
-          <img src={logo} alt="homely" />
-        </div>
-      </div>
-
-      <div className="mobile-menu" onClick={() => handleToggle()}>
-        {toggleMenu ? <AiOutlineClose /> : <AiOutlineMenu />}
-      </div>
-
-     
-        <div className={toggleMenu?"mobile-buttons":"buttons-section"}>
-          <button><a href="#about">About Us</a></button>
-          <button ><a href="#articles">Articles</a></button>
-          <button><a href="#property">Property</a></button>
-          <button><a href="#car-rental">Dropoffs</a></button>
-          <button onClick={()=>setGetStarted(true)} >Get Started</button>
+    <div className={isBackgroundColored?"background-colored":""}>
+      <nav className={"nav-bar-container "} id="navBar">
+        <div className="logo-section">
+          <div className="logo">
+            <img src={logo} alt="homely" />
+          </div>
         </div>
 
+        <div className="mobile-menu" onClick={() => handleToggle()}>
+          {toggleMenu ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+
+        <div className={toggleMenu ? "mobile-buttons" : "buttons-section"}>
+          <button>
+            <a href="#about">About Us</a>
+          </button>
+          <button>
+            <a href="#articles">Articles</a>
+          </button>
+          <button>
+            <a href="#property">Property</a>
+          </button>
+          <button>
+            <a href="#car-rental">Dropoffs</a>
+          </button>
+          <button onClick={() => setGetStarted(true)}>Get Started</button>
+        </div>
 
         <div className="modals">
-         {
-          getStarted?(
-            <Prompt handleCloseModal={()=>setGetStarted(false)}/>
-          ):null
-         }
+          {getStarted ? (
+            <Prompt handleCloseModal={() => setGetStarted(false)} />
+          ) : null}
         </div>
-     
-    </nav>
+      </nav>
+    </div>
   );
 }
